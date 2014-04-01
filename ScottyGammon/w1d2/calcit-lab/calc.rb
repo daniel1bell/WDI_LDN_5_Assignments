f3c0d1acb8477c2a05921843d7f55734e0f9350d
ScottyGams <ScottyGammon@gmail.com>
2014-04-01 16:38:28 +0100

    ruby coding excersizes

--- ScottyGammon/w1d2/calcit-lab/calc.rb
+++ ScottyGammon/w1d2/calcit-lab/calc.rb
@@ -6,22 +6,56 @@ def menu
   print "(b)asic, (a)dvanced, or (q)uit: "
   gets.chomp.downcase
 end
- 
+
+def runagain
+  puts "whould you like to run again (y/n?)"
+  answer=gets.chomp
+  if answer=='y'
+    menu
+  else quit
+  end
+end
+# def number_1
+#  # ask for the first number
+#  print "First Number? "
+#  num1 = gets.chomp.to_f
+# end
+
+#  def number_2
+#    # ask for the second number
+#    print "Second Number? "
+#    num2 = gets.chomp.to_f
+# end
+
 def basic_calc
   # ask the user which operation they want to perform
   print "(a)dd, (s)ubtract, (m)ultiply, (d)ivide: "
   operation = gets.chomp.downcase
- 
-  # insert magic incatation here....
-  puts "gotta replace this with some code to do the '#{operation}' operation..."
- 
+
+  print "First Number? "
+  num1 = gets.chomp.to_f
+  print "Second Number? "
+  num2 = gets.chomp.to_f
+
+case operation
+
+when "a"
+  puts  num1 + num2
+when "s"
+  puts num1 - num2
+when "m"
+  puts num1 * num2
+when "d"
+  puts num1 / num2
 end
- 
- 
+
+
+end
+
+
 # run the app...
- 
-response = menu
- 
+response = runagain
+response = runagain
 while response != 'q'
   case response
   when 'b'
@@ -29,6 +63,4 @@ while response != 'q'
   when 'a'
     advanced_calc
   end
- 
-  response = menu
 end
