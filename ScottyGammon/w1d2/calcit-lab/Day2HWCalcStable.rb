4c2f27855c22e1633af56e23acc723f0a3990f85
ScottyGams <ScottyGammon@gmail.com>
2014-04-02 08:43:07 +0100

    Did my homework. LOLZ

--- ScottyGammon/w1d2/calcit-lab/Day2HWCalcStable.rb
+++ ScottyGammon/w1d2/calcit-lab/Day2HWCalcStable.rb
@@ -3,7 +3,7 @@ def menu
   # Clear the screen, and present the user with a menu
   puts `clear`
   puts "***CalcIt***"
-  print "(b)asic, (a)dvanced, or (q)uit: "
+  print "(b)asic, (a)dvanced, (s)ituationor (q)uit: "
   gets.chomp.downcase
 end
 
@@ -24,26 +24,26 @@ end
 # end
 
 def basic_calc
-  # ask the user which operation they want to perform
   print "(a)dd, (s)ubtract, (m)ultiply, (d)ivide: "
   operation = gets.chomp.downcase
-
-  print "First Number? "
-  num1 = gets.chomp.to_f
-  print "Second Number? "
-  num2 = gets.chomp.to_f
+  print "first number: "
+  a = gets.to_f
+  print "second number: "
+  b = gets.to_f
 
   case operation
-
-  when "a"
-    puts  num1 + num2
-  when "s"
-    puts num1 - num2
-  when "m"
-    puts num1 * num2
-  when "d"
-    puts num1 / num2
+  when 'a'
+    puts "#{a + b}"
+  when 's'
+    puts "#{a - b}"
+  when 'm'
+    puts "#{a * b}"
+  when 'd'
+    puts "#{a / b}"
   end
+
+  gets
+end
 
 def advanced_calc
   print "(p)ower, (s)qrt: "
@@ -62,9 +62,22 @@ def advanced_calc
   gets
 end
 
+def mort_calc
+  print "How much is your house?"
+  house = gets.chomp.to_i
+  print "How long is the loan? (months)"
+  months = gets.chomp.to_i
+  print = "What is the interest rate?"
+  rate = gets.chomp.to_f
+  rate / 100
 
+  looked_up_math = (1+rate/12)**(12/12)-1
+  annuity = (1-(1/(1+looked_up_math))**months)/looked_up_math
+
+  payment = house/annuity
+
+  puts "You will pay #{payment} per month"
 end
-
 
 # run the app...
 
@@ -76,6 +89,8 @@ while response != 'q'
     basic_calc
   when 'a'
     advanced_calc
+  when 's'
+    mort_calc
   end
 
   # if runagain?
