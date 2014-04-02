515d84ed9b441e0ac7cd5c5b336fb35d3710ae89
W-Sarah <sarahweiss75@gmail.com>
2014-04-02 21:29:23 +0100

    changing the hash/array selection in mta and adding a case if user select union square

--- Sarah/w1d3/mta/mta.rb
+++ Sarah/w1d3/mta/mta.rb
@@ -1,4 +1,4 @@
-all_stops = { "ts" => "n_line", "34th" => "n_line", "28th-n" => "n_line", "23rd-n" => "n_line","us-n"=> "n_line","8th-n" => "n_line", "8th-l" => "l_line","6th"=> "l_line","us-l"=> "l_line", "3rd"=> "l_line","1st"=> "l_line","gc" => "s_line", "33rd" => "s_line", "28th-s" => "s_line", "23rd-s"=> "s_line", "us-s"=> "s_line", "ap" => "s_line"}
+all_stops = { "n_line" => ["ts" , "34th" , "28th-n", "23rd-n","us", "8th-n"], "l_line" => ["8th-l","6th","us", "3rd","1st"], "s_line" => ["gc", "33rd", "28th-s", "23rd-s", "us", "ap"]}
 lines = ['n_line', "l_line", "s_line"]
 
 # getting on line
@@ -8,12 +8,7 @@ while lines.include?(on_user_input) == false
   puts "Please enter n_line, s_line or l_line"
   on_user_input = gets.chomp
 end
-on_line = []
-all_stops.each do |stop,line|
-  if line == on_user_input
-    on_line.push(stop)
-  end
-end
+on_line = all_stops[on_user_input]
 
 # getting on stop
 print "Which stop would you like to get on: "
@@ -33,12 +28,7 @@ while lines.include?(off_user_input) == false
   puts "Please enter n_line, s_line or l_line"
   off_user_input = gets.chomp
 end
-off_line = []
-all_stops.each do |stop,line|
-  if line == off_user_input
-    off_line.push(stop)
-  end
-end
+off_line = all_stops[off_user_input]
 
 # getting off stop
 print "Which stop would you like to get on: "
@@ -51,8 +41,13 @@ while off_line.include?(off_stop) == false
   off_stop = gets.chomp
 end
 
-
 if on_user_input == off_user_input
+  number_stops = (on_line.index(off_stop) - on_line.index(on_stop)).abs
+
+elsif on_user_input != off_user_input && on_stop == "us"
+  number_stops = (off_line.index(off_stop) - off_line.index(on_stop)).abs
+
+elsif on_user_input != off_user_input && off_stop == "us"
   number_stops = (on_line.index(off_stop) - on_line.index(on_stop)).abs
 
 else
