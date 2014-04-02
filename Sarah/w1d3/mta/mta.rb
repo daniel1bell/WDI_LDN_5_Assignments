863ad3cc4c66023ccd0daf4d678aa0f937765372
W-Sarah <sarahweiss75@gmail.com>
2014-04-02 19:51:53 +0100

    adding safeguards for user prompts on mta file

--- Sarah/w1d3/mta/mta.rb
+++ Sarah/w1d3/mta/mta.rb
@@ -1,10 +1,13 @@
-all_stops = { "ts" => "n_line", "34th" => "n_line", "28th-n" => "n_line", "23rd-n" => "n_line","us-n"=> "n_line","8th-n" => "n_line", "8th-l" => "l_line","6th"=> "l_line","us-l"=> "l_line", "3rd"=> "l_line","1st"=> "l_line"}
-n_line = [ "ts", "34th", "28th", "23rd-n","us","8th" ]
-l_line = ["8th","6th","us", "3rd","1st"]
+all_stops = { "ts" => "n_line", "34th" => "n_line", "28th-n" => "n_line", "23rd-n" => "n_line","us-n"=> "n_line","8th-n" => "n_line", "8th-l" => "l_line","6th"=> "l_line","us-l"=> "l_line", "3rd"=> "l_line","1st"=> "l_line","gc" => "s_line", "33rd" => "s_line", "28th-s" => "s_line", "23rd-s"=> "s_line", "us-s"=> "s_line", "ap" => "s_line"}
+lines = ['n_line', "l_line", "s_line"]
 
 # getting on line
-puts "Which line would you like to get on? n_line, l_line?"
+puts "Which line would you like to get on? n_line, l_line, s_line?"
 on_user_input = gets.chomp
+while lines.include?(on_user_input) == false
+  puts "Please enter n_line, s_line or l_line"
+  on_user_input = gets.chomp
+end
 on_line = []
 all_stops.each do |stop,line|
   if line == on_user_input
@@ -18,10 +21,18 @@ on_line.each { |stop| print "#{stop}, " }
 print "?"
 puts ""
 on_stop = gets.chomp
+while on_line.include?(on_stop) == false
+  puts "Please enter the name of the stop you want to get on from the list above"
+  on_stop = gets.chomp
+end
 
 # getting off line
-puts "Which line would you like to get off? n_line, l_line?"
+puts "Which line would you like to get off? n_line, l_line, s_line?"
 off_user_input = gets.chomp
+while lines.include?(off_user_input) == false
+  puts "Please enter n_line, s_line or l_line"
+  off_user_input = gets.chomp
+end
 off_line = []
 all_stops.each do |stop,line|
   if line == off_user_input
@@ -35,6 +46,11 @@ off_line.each { |stop| print "#{stop}, " }
 print "?"
 puts ""
 off_stop = gets.chomp
+while off_line.include?(off_stop) == false
+  puts "Please enter the name of the stop you want to get off from the list above"
+  off_stop = gets.chomp
+end
+
 
 if on_user_input == off_user_input
   number_stops = (on_line.index(off_stop) - on_line.index(on_stop)).abs
@@ -45,7 +61,7 @@ on_line.collect! { |stop|
   (stop.match("us")) ? "on_union_square" : stop
 }
 
- #union square station of on_line
+ #union square station of off_line
 off_line.collect! { |stop|
   (stop.match("us")) ? "off_union_square" : stop
 }
