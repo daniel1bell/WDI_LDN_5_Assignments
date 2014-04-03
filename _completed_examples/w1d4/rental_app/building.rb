95f1ca3f89f980f7afe18714dce6961ffdca3a06
Pavling <pavling@gmail.com>
2014-04-03 23:03:40 +0100

    adding rental-app example code

---
+++ _completed_examples/w1d4/rental_app/building.rb
@@ -0,0 +1,63 @@
+class Building
+  attr_accessor :post_code, :affluency
+  attr_reader :flats, :prospects
+
+  def initialize(post_code)
+    @post_code = post_code
+    @flats = {}
+    @prospects = {}    
+  end
+
+  def vacant_flats
+    flats.select { |key,flat| flat.vacant? }
+  end
+
+  def occupied_flats
+    flats.reject { |key,flat| flat.vacant? }
+  end
+
+  def list_flats
+    if flats.any?
+      flats.values.join("\n")
+    else
+      "There are no flats at the moment.\nGo drum up some landlords!"
+    end
+  end
+
+  def tenants
+    flats.values.inject({}) { |tenants, flat| tenants.merge(flat.tenants)}
+  end
+
+  def list_tenants
+    if tenants.any?
+      tenants.keys.join(", ")
+    else
+      "There are no tenants at the moment.\nGo let some flats!"
+    end
+  end
+
+  def list_vacant_flats
+    if vacant_flats.any?
+      vacant_flats.keys.join(', ')
+    else
+      "There are no vacant flats.\nGo get some more on the books!"
+    end
+  end
+
+  def list_occupied_flats
+    if occupied_flats.any?
+      occupied_flats.keys.join(', ')
+    else
+      "There are no occupied flats.\nGet some tenants, or you're looking for a new job!"
+    end
+  end
+
+  def list_prospects
+    if prospects.any?
+      prospects.keys.join(', ')
+    else
+      "There are no prospects.\nGet out there and drag people in off the street if you have to!"
+    end
+  end
+
+end
