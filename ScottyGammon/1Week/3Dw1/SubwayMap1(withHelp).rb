0311ac82166cfb00af68eb062c3f9b0813560089
ScottyGams <ScottyGammon@gmail.com>
2014-04-03 10:49:59 +0100

    Renaming my folder structure to make it easier to find in terminal

--- ScottyGammon/1Week/3Dw1/SubwayMap1(withHelp).rb
+++ ScottyGammon/1Week/3Dw1/SubwayMap1(withHelp).rb
@@ -3,9 +3,7 @@ lines = {
   L_Line: ['EighthAlt', 'Sixth', 'UnionSquare', 'Third', 'First'],
   Six_Line: ['GrandCentral', 'ThirtyThird', 'TwentyEighthAlt', 'TwentyThirdAlt', 'UnionSquare', 'AstorPlace']
 }
-
 =begin
-
 Step 1. Ask the user what line they are getting on.
 Step 2. Ask the user what station they are getting on.
 Step 3. Ask user what line they plan on getting off.
@@ -13,7 +11,6 @@ Step 4. Ask user what station they are getting off.
 Step 5. Compare stattions to see if they will be staying on the same line
 Step 6. If there is a transfer, intersect at Union Square and transfer them, continueing to count.
 Step 7. Count up the stops and continue
-	
 =end
 
 def prompt_user(message)
@@ -29,19 +26,19 @@ end
 
 start_line = prompt_user("Which line are you getting on? Options are N_Line, L_Line, Six_Line").to_sym
 
-if line.has_key?(start_line)
+if lines.has_key?(start_line)
 
 	lines[:N_Line].each do |line|
 		puts "The list of stations in N Line is: #{line}"
 	end
 end
-if line.has_key?(start_line)
+if lines.has_key?(start_line)
 
 	lines[:L_Line].each do |line|
 		puts "The list of stations in L Line is: #{line}"
 	end
 end
-if line.has_key?(start_line)
+if lines.has_key?(start_line)
 
 	lines[:Six_Line].each do |line|
 		puts "The list of stations in Six Line is: #{line}"
@@ -58,4 +55,3 @@ end_distance = how_many_stops(lines, end_line, end_station)
 total_distance = (start_distance + end_distance).abs #Thanks to Sarah for telling me about .abs
 
 puts "You will travel a total of #{total_distance} stops on your trip."
-
