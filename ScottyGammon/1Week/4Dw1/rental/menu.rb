f67c7af908c38b62b88dddc21dbb36b17208b167
ScottyGams <ScottyGammon@gmail.com>
2014-04-03 23:06:00 +0100

    Played with code making things work.

--- ScottyGammon/1Week/4Dw1/rental/menu.rb
+++ ScottyGammon/1Week/4Dw1/rental/menu.rb
@@ -5,7 +5,7 @@ def menu
 	puts "2: List Prospects"
 	puts "3: Let a Flat"
 	puts "4: Evict a TenantX"
-	puts "5: List Empty Flats"
+	puts "5: List Empty FlatsA"
 	puts "6: List Tenants"
 	puts "7: Create a Prospect"
 	puts "8: Create a Flat"
@@ -37,25 +37,10 @@ def let_flat(building) #pass the building into this method
 		puts "Either flat or prospect is invalid"
 	end
 end
-=begin
-def let_flat(building) #pass the building into this method
-	print 'Which flat?'
-	print "(#{building.list_empty_flats})"
-	flat_name = gets.chomp
-	print 'Which prospect?'
-	#print "(#{prospect.list_empty_flats})"
-	prospect_name = gets.chomp.downcase
 
-
-
-	prospect = building.prospects.delete(prospect_name)
-	flat = building.flats[flat_name]
-
-	if prospect && flat && flat.vacant?
-		flat.tenants[prospect.name] = prospect
-		puts "You have placed #{prospect_name} into #{flat_name}."
-	else
-		puts "Either flat or prospect is invalid"
-	end
+def evict_tenant #pass the building into this method
+	print 'Which tenant?'
+	tenant_name = gets.chomp.downcase
+	tenant = flat.tenants.delete(tenant_name)
+	
 end
-=end
