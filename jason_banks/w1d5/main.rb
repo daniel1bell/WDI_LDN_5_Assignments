1d985156ef73ce441cd75256f9ef3abdfedcf1f4
jasonklein <jasonklein@gmail.com>
2014-04-05 11:29:24 +0100

    Create files for investment homework.

---
+++ jason_banks/w1d5/main.rb
@@ -0,0 +1,5 @@
+require "pry"
+
+require_relative "menu"
+require_relative "client"
+require_relative "portfolio"
---
+++ jason_banks/w1d5/menu.rb
@@ -0,0 +1,11 @@
+def menu
+  puts "*** SuperTrustee Investments ***"
+  puts "\n\n'Carrying clients before we carry ourselves since 1901.'\n\n"
+  puts "List Clients"
+  puts "Sell Stock for a Client"
+  puts "Buy Stock for a Client"
+  puts "Move Stock between a Client's Portfolios"
+  puts "Get a Stock Price"
+  puts "Quit"
+end
+
