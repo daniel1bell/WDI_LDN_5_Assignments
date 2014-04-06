6938d1f1dfab822572b6396f9016496ec87500c8
hansss <hanakim@college.harvard.edu>
2014-04-06 17:43:22 +0100

    First draft of buy stocks finished.

--- Hana/w1d5/stocks/firm.rb
+++ Hana/w1d5/stocks/firm.rb
@@ -10,16 +10,34 @@ class Firm
   end
 
   def buy_stocks
+    print "For which client?\n"
+    print "[#{clients.keys.join(", ")}]\n"
+    client_buying_for = gets.chomp
+
+    print "Which portfolio? \n"
+    print "[#{clients[chosen_client].portfolio.keys.join(", ")}] \n"
+    chosen_portfolio = gets.chomp
+
     print "What stock would you like to buy?\n"
     stock_name = gets.chomp.upcase!
+    
     print "How many shares would you like to buy?\n"
     quantity_of_shares = gets.chomp
 
-
     stock_price = YahooFinance::get_standard_quotes('stock_name')['stock_name'].lastTrade
 
+    cost_of_stock_purchase = (quantity_of_shares - stock_price).round(2)
 
+    if clients[client_buying_for].balance >= cost_of_stock_purchase
 
+      clients[client_buying_for].balance -= cost_of cost_of_stock_purchase
+      
+      clients.fetch(client_buying_for).portfolio.stocks[stock_name] = Stock.new(stock_name, stock_price, quantity_of_shares)
+    else
+      prints "You do not have enough money to buy that. Sorry about that."
+    end
+
+    
   end
 
   def list_client_info
