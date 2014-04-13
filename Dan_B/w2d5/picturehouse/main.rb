8508a9f00f3a17d9a2c37519ea7b107750aca909
daniel1bell <daniel1bell@me.com>
2014-04-13 19:26:50 +0100

    Working code and fully styled. Task complete.

--- Dan_B/w2d5/picturehouse/main.rb
+++ Dan_B/w2d5/picturehouse/main.rb
@@ -16,10 +16,50 @@ get '/' do
   sql = "SELECT * FROM movies"
   @all_movies = @db.exec(sql)
 
+  sql_cat = "SELECT * FROM categories"
+  @categories = @db.exec(sql_cat)
+
+  erb :list
+end
+  
+get '/list/:name' do
+  category = params[:name]
+  sql = "SELECT * FROM movies WHERE category = '#{category}'"
+  @all_movies = @db.exec(sql)
+
+  sql_cat = "SELECT * FROM categories"
+  @categories = @db.exec(sql_cat)
+
   erb :list
 end
 
+get '/categories' do
+  sql_cat = "SELECT * FROM categories"
+  @categories = @db.exec(sql_cat)
+
+  erb :categories
+end
+
+post '/create/category' do
+  name = params[:name].split.map(&:capitalize).join(' ')
+  description = params[:description]
+
+  sql = "INSERT INTO categories (name, description) values (#{sql_string(name)}, #{sql_string(description)})"
+
+  @db.exec(sql)
+  redirect to('/categories')
+end
+
+post '/delete/category/:id' do
+  sql = "DELETE FROM categories WHERE id = #{params[:id].to_i}"
+  @db.exec(sql)
+  redirect to('/categories')
+end
+
 get '/new' do
+  sql_cat = "SELECT * FROM categories"
+  @categories = @db.exec(sql_cat)
+
   erb :new_vid
 end
 
@@ -40,6 +80,9 @@ get '/edit/:id' do
   id = params[:id].to_i
   sql = "SELECT * FROM movies WHERE id=#{id}"
   @movies = @db.exec(sql)
+
+  sql_cat = "SELECT * FROM categories"
+  @categories = @db.exec(sql_cat)
 
   erb :edit
 end
@@ -62,9 +105,9 @@ post '/delete/:id' do
   redirect to('/')
 end
 
-# def sql_string(value)
-#   "'#{value.gsub("'", "''")}'"
-# end
+def sql_string(value)
+  "'#{value.gsub("'", "''")}'"
+end
 class String
   def t_format
     self[0,16]
--- Dan_B/w2d5/picturehouse/picturehouse.sql
+++ Dan_B/w2d5/picturehouse/picturehouse.sql
@@ -1,3 +1,6 @@
+-- CREATE DATABASE picturehouse_app;
+
+DROP TABLE movies;
 CREATE TABLE movies
 (
   id serial4 primary key,
@@ -8,3 +11,26 @@ CREATE TABLE movies
   created_on timestamp,
   updated_on timestamp
 );
+
+DROP TABLE categories;
+CREATE TABLE categories
+(
+  id serial4 primary key,
+  name varchar(100),
+  description text
+);
+
+INSERT INTO categories (name, description) values ('Educations Models', 'Clips showing emerging models of educations and how they intend to change the way we approach education.');
+INSERT INTO categories (name, description) values ('Teaching Methodology', 'Including new approaches to the methodology of education.');
+INSERT INTO categories (name, description) values ('Other', 'A category for all entries that may not fit perfectly into one of the others.');
+
+
+INSERT INTO movies (title, category, description, url, created_on) values ('The Academy For Global Citizenship', 'Educations Models', 'The Academy for Global Citizenship in Chicago teaches kids about a grander scope than bynomials, Jane Austin or mitochondria: it teaches them how to be contributing citizens of a global society. Run by Sarah Elizabeth Ippel and her team - it''s a brilliant example of emerging education at its finest.', 'http://www.youtube.com/watch?feature=player_embedded&v=rhnyF3jP5ak', current_timestamp);
+
+INSERT INTO movies (title, category, description, url, created_on) values ('Do Schools Kill Creativity?', 'Teaching Methodology', 'Ken Robinson explains his thoughts on creativity in education. He thinks that the modern school system isn''t doing great things for nourishing creativity.', 'http://www.youtube.com/watch?feature=player_embedded&v=iG9CE55wbtY', current_timestamp);
+
+INSERT INTO movies (title, category, description, url, created_on) values ('Shelter From The Storm', 'Teaching Methodology', 'Maria Escudié Reifler is a one of a kind teacher. Just watch to see the love she brings to a classroom and the profound affect it has on the kids.', 'http://www.youtube.com/watch?feature=player_embedded&v=7L83pTjnBZ0', current_timestamp);
+
+INSERT INTO movies (title, category, description, url, created_on) values ('What If Money Was No Object?', 'Other', 'Alan Watts ponders on how controlled our destiny''s are by the fear of making or not making money. He asks what you would do instead of what you''re doing now if money in fact wasn''t a cause for concern. Would it be different?', 'http://www.youtube.com/watch?feature=player_embedded&v=je3rQevW-cw', current_timestamp);
+
+INSERT INTO movies (title, category, description, url, created_on) values ('Democratic Education: Schools Of Trust', 'Educations Models', 'The Schules des Leibens in Germany believe in the principals of democratic education. In this system, curriculum design is done by the kids and teachers become a master of learning instead of being the master of knowledge.', 'http://www.youtube.com/watch?feature=player_embedded&v=pfHrgRITnVM', current_timestamp);
