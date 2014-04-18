d54f44ad15b547926c5f37516a9880bf224be347
daniel1bell <daniel1bell@me.com>
2014-04-18 10:56:28 +0100

    Tidied up Engine CRUD now working. Tidied up CSS Asset Pipeline.

--- Dan_B/w3d3/car_dealership_app/app/controllers/engines_controller.rb
+++ Dan_B/w3d3/car_dealership_app/app/controllers/engines_controller.rb
@@ -21,23 +21,28 @@ class EnginesController < ApplicationController
   def create
     @engine = Engine.create(params[:engine])
 
-    redirect_to 'engines#index'
+    redirect_to(engines_path)
   end
 
   def update
     @engine = Engine.find(params[:id])
 
-    redirect_to 'engines#index'
+    respond_to do |format|
+      if @engine.update_attributes(params[:engine])
+        format.html { redirect_to engines_path, notice: 'Engine was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @engine.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
     @engine = Engine.find(params[:id])
     @engine.destroy
 
-    respond_to do |format|
-      format.html { redirect_to 'engines#index' }
-      format.json { head :no_content }
-    end
+    redirect_to(engines_path)
   end
 
 end
