5dcc63e5c2bc0ec872cf67d08bfc466f2d179bca
daniel1bell <daniel1bell@me.com>
2014-05-06 01:10:41 +0100

    Working basic calc, mortgage calc and bmi calc with real time updating.

--- Dan_B/w6d1/CalcIt JS/main.js
+++ Dan_B/w6d1/CalcIt JS/main.js
@@ -1,30 +1,112 @@
-function update() {
+function numberCheck(input) {
+  return !input.value == false && !isNaN(parseFloat(input.value));
+}
+
+function roundNumber(number) {
+  if (number % 1 > 0) {
+    return parseFloat(number.toFixed(2));
+  } else {
+    return number;
+  }
+}
+
+function printAnswer(css_id, answer) {
+  var answerSpace = document.getElementById(css_id);
+  answerSpace.innerHTML = "= " + roundNumber(answer)
+}
+
+
+function basicUpdate() {
   var num1 = document.getElementById('number1');
   var num2 = document.getElementById('number2');
   var operator = document.getElementById('operator').value;
 
   if (operator == "Square Root") {
-    num2.setAttribute('style', 'display: none;')
+    num2.setAttribute('style', 'display: none;');
   } else if (operator != "Square Root") {
-    num2.setAttribute('style', 'display: inline;')
+    num2.setAttribute('style', 'display: inline;');
   }
 
-    var answerSpace = document.getElementById('answer')
-
-    var a = parseFloat(num1.value)
-    var b = parseFloat(num2.value)
+  if (numberCheck(num1) && numberCheck(num2) == false) {
+    var a = parseFloat(num1.value);
+  
+    if (operator == "Square Root") {
+      var result = Math.sqrt(a);
+      printAnswer('answer', result);
+    }
+  } else if (numberCheck(num1) && numberCheck(num2)) {
+    var a = parseFloat(num1.value);
+    var b = parseFloat(num2.value);
 
     if (operator == "+") {
-      var answer = a + b;
+      var result = a + b;
+      printAnswer('answer', result);
     } else if (operator == "-") {
-      var answer = a - b;
+      var result = a - b;
+      printAnswer('answer', result);
     } else if (operator == "*") {
-      var answer = a * b;
+      var result = a * b;
+      printAnswer('answer', result);
     } else if (operator == "/") {
-      var answer = a / b;
-    } else if (operator == "Square Root") {
-      var answer = Math.sqrt(a);
+      var result = a / b;
+      printAnswer('answer', result);
+    }
+  }
+}
+
+function mortgageUpdate() {
+  var principal = document.getElementById('principal');
+  var rate = document.getElementById('rate');
+  var years = document.getElementById('mortgage_length_years');
+
+  if (numberCheck(principal) && numberCheck(rate) && numberCheck(years)) {
+    var p = parseFloat(principal.value);
+    var r = parseFloat(rate.value) / 100 / 12;
+    var l = parseFloat(years.value) * 12;
+
+    var result = p * (r * (Math.pow((1 + r), l))) / ((Math.pow(1 + r, l)) -1);
+
+    var answerSpace = document.getElementById('monthly_payments');
+    answerSpace.innerHTML = "= \u00A3" + roundNumber(result)
+  }
+}
+
+function bmiUpdate() {
+  var weight = document.getElementById('weight');
+  var height = document.getElementById('height');
+  var weight_measure = document.getElementById('weight_measure');
+  var height_measure = document.getElementById('height_measure');
+
+  if (numberCheck(weight) && numberCheck(height)) {
+    if (weight_measure.checked) {
+      var w = parseFloat(weight.value);
+    } else {
+      var w = parseFloat(weight.value) * 0.453592;
     }
 
-    answerSpace.innerHTML = "= " + answer
+    if (height_measure.checked) {
+      var h = parseFloat(height.value) / 100;
+    } else {
+      var h = parseFloat(height.value) / 0.393700787 / 100;
+    }
+
+    var result = w / Math.pow(h, 2);
+    printAnswer('bmi_result', result)
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
